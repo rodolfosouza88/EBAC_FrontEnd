ebc2b2adc662958b58fec13cf404191feda8fc38
rodolfosouza88 <rodolfovasconcellos88@gmail.com>
2024-09-18 11:08:44 -0300

    projeto finalizado

---
+++ 6.0 - Outros recursos do JavaScript/Ajax e excecoes/main.js
@@ -0,0 +1,26 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('btn-buscar-cep').addEventListener('click', function (){
+//         const xhttp = new XMLHttpRequest();
+//         const cep = document.getElementById('cep').value;
+//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//         xhttp.open('GET', endpoint);
+//         xhttp.send();
+//     })
+// })
+
+$(document).ready(function() {
+    $('#btn-buscar-cep').click(function(){
+        const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        $.ajax(endpoint).done(function (resposta) {
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro}, ${cidade}, ${estado}`;
+            $('#endereco').val(endereco);
+        })
+    })
+})
